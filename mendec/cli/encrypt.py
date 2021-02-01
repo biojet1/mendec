e12b25752e31b5c5ef8a89770608d90db166f651
biojet1 <biojet1@gmail.com>
2021-02-01 18:50:00 +0800

    prepare v0.0.1

--- mendec/cli/encrypt.py
+++ mendec/cli/encrypt.py
@@ -7,35 +7,18 @@ from .pick import Crypt
 @arg("message", default=None, help="the message file")
 @arg("key", required=True, help="the key file")
 class Encrypt(Crypt, BasicLog, Main):
-    log_level = "INFO"
-    app_name = "encrypt"
-
     def start(self, *args, **kwargs):
-        from ..message import encrypt
-        from .pick import parse_keyfile, write_to, read_from, as_source, as_sink
+        from .pick import parse_keyfile, as_source, as_sink
 
         # parse the key file
         desc = parse_keyfile(self.key)
         # get n, e, d
         e = desc["e"] if "e" in desc else desc["d"]
         if self.short:
-            # get message
-            message = read_from(self.message)
-            # encrypt
-            encrypted = encrypt(message, desc["n"], e)
-            if self.encoding == "ub64":
-                from base64 import urlsafe_b64encode as encode
+            from ..message import encrypt
 
-            elif self.encoding == "b64":
-                from base64 import b64encode as encode
-
-            elif self.encoding == "hex":
-                from binascii import hexlify as encode
-            else:
-                encode = None
-
-            # output
-            write_to(self.output, encode(encrypted) if encode else encrypted)
+            with as_source(self.message) as r, as_sink(self.output) as w:
+                w.write(encrypt(r.read(), desc["n"], e))
         else:
             from ..message import vencrypt
 
--- mendec/cli/test_cli.py
+++ mendec/cli/test_cli.py
@@ -1,8 +1,21 @@
 import unittest
 from subprocess import call
+from hashlib import md5
 
 
 class Test(unittest.TestCase):
+    def shell_ok(self, cmd):
+        self.assertEqual(0, call(cmd, shell=True), msg=cmd)
+
+    def shell_fail(self, cmd):
+        self.assertNotEqual(0, call(cmd, shell=True), msg=cmd)
+
+    def same_file(self, f1, f2):
+        m1, m2 = md5(), md5()
+        m1.update(open(f1, "rb").read())
+        m2.update(open(f2, "rb").read())
+        self.assertEqual(m1.hexdigest(), m2.hexdigest(), msg="{} {}".format(f1, f2))
+
     def test_usage(self):
 
         self.assertEqual(0, call(r"python -m mendec -h", shell=True))
@@ -14,13 +27,9 @@ class Test(unittest.TestCase):
         self.assertNotEqual(0, call(r"python -m mendec decrypt", shell=True))
         self.assertNotEqual(0, call(r"python -m mendec encrypt", shell=True))
 
-    def check_shell(self, cmd):
-        self.assertEqual(0, call(cmd, shell=True), msg=cmd)
-
     def test_enc_dec(self):
         from os import chdir, urandom
         from tempfile import mkdtemp
-        from hashlib import md5
         from base64 import b64encode
         from string import ascii_letters
 
@@ -30,21 +39,24 @@ class Test(unittest.TestCase):
         with open("MSG", "wb") as h:
             h.write(b64encode(urandom(128 * 1024)))
 
-        self.check_shell("python -m mendec keygen -B96 -p4 -o KEY")
-        self.check_shell("python -m mendec pick KEY 1 KEY1")
-        self.check_shell("python -m mendec pick KEY 2 KEY2")
+        self.shell_ok("python -m mendec keygen -B96 -p4 -o KEY")
+        self.shell_ok("python -m mendec pick KEY 1 KEY1")
+        self.shell_ok("python -m mendec pick KEY 2 KEY2")
         cmd = (
             "printf {0}"
             " | python -m mendec.cli.encrypt {1} --short"
             " | python -m mendec.cli.decrypt {2} --short"
-            r""" | python -c 'import sys; assert(sys.stdin.read() == "{0}")'"""
+            " | python -c 'import sys; assert(sys.stdin.read() == sys.argv[1])' {0}"
         )
-        self.check_shell(cmd.format(ascii_letters, "KEY1", "KEY2"))
-        self.check_shell(cmd.format(ascii_letters, "KEY2", "KEY1"))
-        self.check_shell("python -m mendec encrypt --no-short KEY1 MSG -o ENC")
-        self.check_shell("python -m mendec decrypt --no-short KEY2 ENC -o DEC")
-        msg, dec = md5(), md5()
-        msg.update(open("MSG", "rb").read())
-        dec.update(open("DEC", "rb").read())
-
-        self.assertEqual(dec.hexdigest(), msg.hexdigest())
+        self.shell_ok(cmd.format(ascii_letters, "KEY1", "KEY2"))
+        self.shell_ok(cmd.format(ascii_letters, "KEY2", "KEY1"))
+        self.shell_fail(cmd.format(ascii_letters, "KEY2", "KEY2"))
+        self.shell_fail(cmd.format(ascii_letters, "KEY1", "KEY1"))
+        self.shell_ok("python -m mendec encrypt --no-short KEY1 MSG -o ENC1")
+        self.shell_ok("python -m mendec decrypt --no-short KEY2 ENC1 -o DEC1")
+        self.same_file("MSG", "DEC1")
+        self.shell_fail("python -m mendec decrypt --no-short KEY1 ENC1 -o DEC1")
+        self.shell_ok("python -m mendec encrypt --no-short KEY2 MSG -o ENC2")
+        self.shell_ok("python -m mendec decrypt --no-short KEY1 ENC2 -o DEC2")
+        self.same_file("MSG", "DEC2")
+        self.shell_fail("python -m mendec decrypt --no-short KEY2 ENC2 -o DEC2")
